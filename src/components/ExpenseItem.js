aca5cee1a2c1407995e5fe98371feca317c5d475
SonuSangam7 <sonusangamcsk1@gmail.com>
2022-11-25 12:06:46 +0530

    firstcomponent

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,8 @@
+function ExpenseItem(){
+    return <div><div>Expense Item</div>
+    <div>Food Rs 10</div>
+    <div>Food Rs 100</div>
+    <div>Food Rs 200</div>
+    </div>;
+}
+export default ExpenseItem;
