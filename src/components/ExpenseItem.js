580a12c061bc26b3c42fb46542e73fa100e0d88d
SonuSangam7 <sonusangamcsk1@gmail.com>
2022-12-03 12:49:19 +0530

    form added in react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,23 @@
 import ExpenseDate from './ExpenseDate';
+import React,{useState} from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title,setTitle]= useState(props.title);
+
+  const clickhandler=()=>{
+    setTitle('updated!');
+    console.log(title)
+  }
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickhandler}> Change title</button>
+      <button > Delete</button>
     </div>
   );
 }
